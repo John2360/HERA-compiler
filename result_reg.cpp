44b91d0c5b14f8652e7bc81c06c71a41c3ea0cf1
john <jfinberg@haverford.edu>
2022-04-28 12:31:04 -0400

    fixed simple_var fp_plus

--- result_reg.cpp
+++ result_reg.cpp
@@ -71,7 +71,7 @@ int A_varExp_::init_result_fp_plus() {
     int for_me = this->parent()->fp_plus_for_me(this);
 
     if (for_me == -1){
-        return this->parent()->regular_fp_plus();
+        return this->parent()->result_fp_plus();
     } else {
         return for_me;
     }
